df9465d026824a027460207eea00900943c527a0
Rizky Fadillah <fadillahrizki16@gmail.com>
2023-06-05 20:37:00 +0700

    fix product and remake search compoentn

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -20,112 +20,122 @@ class ProductCard extends StatefulWidget {
 class _ProductCardState extends State<ProductCard> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
-        decoration: BoxDecoration(
-          border: Border.all(color: CustomColor().secondary, width: 1),
-          borderRadius: BorderRadius.circular(8),
-          color: CustomColor().white,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Expanded(
-                flex: 2,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 12.0),
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundColor: CustomColor().primary,
-                    child: Text(
-                      widget.itemImage,
-                      style:
-                          TextStyle(color: CustomColor().white, fontSize: 12),
-                    ),
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: const EdgeInsets.only(left: 20, top: 10, bottom: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: CustomColor().secondary, width: 1),
+        borderRadius: BorderRadius.circular(8),
+        color: CustomColor().white,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: CustomColor().primary,
+                child: Text(
+                  widget.itemImage,
+                  style: TextStyle(
+                    color: CustomColor().white,
+                    fontSize: 12,
                   ),
-                )),
-            const SizedBox(width: 20),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      widget.itemCode,
-                      style: const TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      widget.itemName,
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      'Rp.${widget.itemPrice}',
-                      style: const TextStyle(
-                        fontSize: 14,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 35,
-                            height: 35,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.rectangle,
-                                color: CustomColor().background,
-                                borderRadius: BorderRadius.circular(8)),
-                            child: IconButton(
-                                onPressed: () {},
-                                icon: const Icon(Icons.remove),
-                                iconSize: 18),
-                          ),
-                          Text(
-                            '${widget.itemStock}',
-                            style: const TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                          Container(
-                            width: 35,
-                            height: 35,
-                            decoration: BoxDecoration(
-                                shape: BoxShape.rectangle,
-                                color: CustomColor().background,
-                                borderRadius: BorderRadius.circular(8)),
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: const Icon(Icons.add),
-                              iconSize: 18,
-                            ),
-                          ),
-                        ]),
-                    const SizedBox(height: 6),
-                  ],
                 ),
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Align(
-                  alignment: Alignment.topRight,
-                  child: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.delete),
-                      iconSize: 28)),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    widget.itemCode,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    widget.itemName,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 6),
+                  Text(
+                    'Rp.${widget.itemPrice}',
+                    style: const TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: CustomColor().background,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.remove),
+                            iconSize: 18,
+                          ),
+                        ),
+                        Text(
+                          '${widget.itemStock}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          width: 35,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.rectangle,
+                            color: CustomColor().background,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: const Icon(Icons.add),
+                            iconSize: 18,
+                          ),
+                        ),
+                      ]),
+                  const SizedBox(height: 6),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete),
+                iconSize: 28,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/products/product_list.dart
+++ lib/screens/products/product_list.dart
@@ -1,6 +1,8 @@
 import 'package:anonymous/components/custom_appBar.dart';
 import 'package:anonymous/components/custom_filterFAB.dart';
+import 'package:anonymous/components/custom_text_field.dart';
 import 'package:anonymous/components/product_card.dart';
+import 'package:anonymous/components/search.dart';
 import 'package:anonymous/constants/custom_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -23,122 +25,83 @@ class _ListProductState extends State<ListProduct> {
       backgroundColor: CustomColor().background,
       appBar: const CustomAppBar(title: "Manajemen Product"),
       drawer: const MyDrawer(),
-      body: SingleChildScrollView(
-          physics: const ScrollPhysics(),
-          child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    color: CustomColor().white,
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(color: CustomColor().secondary)),
-                width: 400.0,
-                child: DropdownMenu(
-                  dropdownMenuEntries: [],
-                  hintText: "Pilih Kategori",
-                  menuStyle: MenuStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          CustomColor().white)),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                      width: MediaQuery.of(context).size.width - 125,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 20, bottom: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              labelText: "Cari",
-                              contentPadding: const EdgeInsets.all(8),
-                              filled: true,
-                              fillColor: Colors.white,
-                              focusedBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                  borderSide: BorderSide(
-                                      width: 1.5,
-                                      color: CustomColor().secondary)),
-                              enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                  borderSide: BorderSide(
-                                      width: 1.5,
-                                      color: CustomColor().secondary))),
-                        ),
-                      )),
-                  Container(
-                    padding: const EdgeInsets.all(2.4),
-                    decoration: BoxDecoration(
-                        color: CustomColor().white,
-                        shape: BoxShape.rectangle,
-                        border: Border.all(
-                            width: 1.5, color: CustomColor().secondary),
-                        borderRadius: BorderRadius.circular(8)),
-                    child: IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.search)),
-                  )
-                ],
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.only(left: 25.0, right: 25, bottom: 20),
-                child: ElevatedButton(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: SingleChildScrollView(
+            physics: const ScrollPhysics(),
+            child: Column(
+              children: <Widget>[
+                const Search(),
+                SizedBox(height: 12),
+                ElevatedButton(
                   style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8))),
-                      side: MaterialStateProperty.all(
-                          BorderSide(width: 1, color: CustomColor().secondary)),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          CustomColor().white)),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    side: MaterialStateProperty.all(
+                      BorderSide(
+                        width: 1,
+                        color: CustomColor().secondary,
+                      ),
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      CustomColor().white,
+                    ),
+                  ),
                   onPressed: () {},
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Expanded(
-                          flex: 3,
-                          child: Padding(
-                            padding:
-                                const EdgeInsets.only(top: 12.0, bottom: 12),
-                            child: SizedBox(
-                                width: 100,
-                                height: 100,
-                                child: SvgPicture.asset(assetName)),
-                          )),
+                        flex: 3,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 12.0, bottom: 12),
+                          child: SizedBox(
+                            width: 100,
+                            height: 100,
+                            child: SvgPicture.asset(assetName),
+                          ),
+                        ),
+                      ),
                       Expanded(
                         flex: 4,
                         child: Padding(
-                            padding: const EdgeInsets.only(top: 10, bottom: 8),
-                            child: Text(
-                              "Tambah Produk",
-                              style: TextStyle(
-                                  color: CustomColor().primary,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold),
-                            )),
+                          padding: const EdgeInsets.only(top: 10, bottom: 8),
+                          child: Text(
+                            "Tambah Produk",
+                            style: TextStyle(
+                              color: CustomColor().primary,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ),
-              ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-                itemCount: 10,
-                itemBuilder: (context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 20.0),
-                    child: ProductCard(
+                SizedBox(height: 12),
+                ListView.builder(
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  itemBuilder: (context, int index) {
+                    return Container(
+                      margin: EdgeInsets.only(bottom: 12),
+                      child: ProductCard(
                         itemCode: "A000${index + 1}",
                         itemName: "Produk ${index + 1}",
                         itemPrice: "${(index + 1) * 10000}",
-                        itemStock: index + 1),
-                  );
-                },
-              ),
-            ],
-          )),
+                        itemStock: index + 1,
+                      ),
+                    );
+                  },
+                )
+              ],
+            )),
+      ),
       floatingActionButton: const CustomFAB(),
     );
   }
